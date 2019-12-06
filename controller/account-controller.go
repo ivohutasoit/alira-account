524aaf75249c41db556de3aaaacb427ab84b4d5e
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-06 17:56:33 +0800

    Remove login handler

--- controller/account-controller.go
+++ controller/account-controller.go
@@ -1,14 +1,9 @@
 package controller
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/ivohutasoit/alira-account/model"
-	"github.com/ivohutasoit/alira-account/service"
-	"github.com/ivohutasoit/alira/common"
-	"github.com/ivohutasoit/alira/util"
 )
 
 func RegisterHandler(c *gin.Context) {
@@ -16,21 +11,4 @@ func RegisterHandler(c *gin.Context) {
 		"status": "OK",
 	}
 	c.JSONP(http.StatusOK, data)
-}
-
-func LoginPageHandler(c *gin.Context) {
-	qrcode := &service.QrcodeService{}
-	code := qrcode.Generate()
-	
-	encrypted, err := util.Encrypt(code, common.SecretKey)
-	if err != nil {
-		log.Printf("Error: %s", err.Error())
-	}
-	model.Tokens[code] = model.SocketLogin{
-		Status: 1,
-	}
-
-	c.HTML(http.StatusOK, "login.tmpl.html", gin.H{
-		"code": encrypted,
-	})
 }
