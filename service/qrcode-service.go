012f1920e28074174ba375aebc249819bd80b5f0
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-10 17:47:01 +0800

    Update qrcode-service.go

--- service/qrcode-service.go
+++ service/qrcode-service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -10,11 +12,38 @@ import (
 type QrcodeService struct {
 }
 
-func (qrcode *QrcodeService) Generate() string {
+func (s *QrcodeService) Generate() string {
 	b := make([]byte, 16)
 	rand.Seed(time.Now().UnixNano())
 	for i := range b {
 		b[i] = common.Letters[rand.Intn(len(common.Letters))]
 	}
 	return string(b)
+}
+
+func (s *QrcodeService) Verify(args ...interface{}) (string, error) {
+	if 1 > len(args) {
+		return "", errors.New("not enough parameters")
+	}
+
+	var code, token string
+	for i, p := range args {
+		switch i {
+		case 0:
+			param, ok := p.(string)
+			if !ok {
+				return "", errors.New("string type required")
+			}
+			code = param
+		case 1:
+			param, ok := p.(string)
+			if !ok {
+				return "", errors.New("string type required")
+			}
+			token = param
+		}
+
+	}
+	fmt.Printf("%s %s", code, token)
+	return "", nil
 }
