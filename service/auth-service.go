15d3a7ba27c7f4e3fac0004a30924e9e421a7747
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-11 17:38:56 +0800

    Change dynamic parameter and map return instead

--- service/auth-service.go
+++ service/auth-service.go
@@ -12,25 +12,79 @@ import (
 type AuthService struct {
 }
 
-func (s *AuthService) Login(userid, password string) (string, error) {
-	if userid != "ivohutasoit" {
-		return "", errors.New("invalid user or password")
+func (s *AuthService) Login(args ...interface{}) (map[interface{}]interface{}, error) {
+	if len(args) < 1 {
+		return nil, errors.New("not enough parameters")
+	}
+	authType, ok := args[0].(string)
+	if !ok {
+		return nil, errors.New("plain text parameter not type string")
 	}
 
-	if password != "hutasoit09" {
-		return "", errors.New("invalid user or password")
+	var userid string
+	if authType == "Basic" {
+		if len(args) < 3 {
+			return nil, errors.New("not enough parameters")
+		}
+
+		userid, ok := args[1].(string)
+		if !ok {
+			return nil, errors.New("plain text parameter not type string")
+		}
+		if userid != "ivohutasoit" {
+			return nil, errors.New("invalid user or password")
+		}
+
+		password, ok := args[2].(string)
+		if !ok {
+			return nil, errors.New("plain text parameter not type string")
+		}
+		if password != "hutasoit09" {
+			return nil, errors.New("invalid user or password")
+		}
+
+	} else {
+		if len(args) < 2 {
+			return nil, errors.New("not enough parameters")
+		}
+
+		userid, ok := args[1].(string)
+		if !ok {
+			return nil, errors.New("plain text parameter not type string")
+		}
+		if userid != "ivohutasoit" {
+			return nil, errors.New("invalid user or password")
+		}
 	}
 
-	expiresAt := time.Now().AddDate(0, 0, 7)
-	userToken := &domain.UserToken{
-		UserID: userid,
+	accessTokenClaims := &domain.AccessTokenClaims{
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expiresAt.Unix(),
+			Id:        userid,
+			IssuedAt:  time.Now().Unix(),
+			NotBefore: time.Now().Unix(),
+			ExpiresAt: time.Now().AddDate(0, 0, 7).Unix(),
 			Issuer:    os.Getenv("ISSUER"),
 		},
+		Admin: false,
 	}
-	token := jwt.NewWithClaims(jwt.GetSigningMethod(os.Getenv("HASHING_METHOD")), userToken)
-	tokenString, _ := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	atkn := jwt.NewWithClaims(jwt.GetSigningMethod(os.Getenv("HASHING_METHOD")), accessTokenClaims)
+	accessToken, _ := atkn.SignedString([]byte(os.Getenv("SECRET_KEY")))
 
-	return tokenString, nil
+	refreshTokenClaims := &domain.RefreshTokenClaims{
+		StandardClaims: jwt.StandardClaims{
+			Id:        userid,
+			IssuedAt:  time.Now().Unix(),
+			NotBefore: time.Now().Unix(),
+			ExpiresAt: time.Now().AddDate(0, 0, 8).Unix(),
+			Issuer:    os.Getenv("ISSUER"),
+		},
+		Sub: 1,
+	}
+	rtkn := jwt.NewWithClaims(jwt.GetSigningMethod(os.Getenv("HASHING_METHOD")), refreshTokenClaims)
+	refreshToken, _ := rtkn.SignedString([]byte(os.Getenv("SECRET_KEY")))
+
+	return map[interface{}]interface{}{
+		"access_token":  accessToken,
+		"refresh_token": refreshToken,
+	}, nil
 }
