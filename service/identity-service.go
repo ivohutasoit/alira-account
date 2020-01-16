10d6d8f602f033ddbc49a1b6d69f5e414c26fd81
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-16 17:37:07 +0800

    Add National Code Generator Function

--- service/identity-service.go
+++ service/identity-service.go
@@ -2,11 +2,12 @@ package service
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/ivohutasoit/alira/model"
 	"github.com/ivohutasoit/alira/model/domain"
-	"github.com/ivohutasoit/alira/util"
 )
 
 type IdentityService struct{}
@@ -41,10 +42,11 @@ func (s *IdentityService) CreateNationIdentity(args ...interface{}) (map[interfa
 			return nil, errors.New("identity has used other user")
 		}
 	} else {
+		code, _ := GenerateNationalCode(args[4].(string), args[5].(time.Time))
 		identity = &domain.Identity{
 			Class:  "NATION",
 			UserID: user.BaseModel.ID,
-			Code:   util.GenerateNationalCode(args[4].(string), args[5].(time.Time)),
+			Code:   code.(string),
 		}
 		model.GetDatabase().Create(&identity)
 	}
@@ -66,4 +68,50 @@ func (s *IdentityService) CreateNationIdentity(args ...interface{}) (map[interfa
 		"identity_code": identity.Code,
 		"userid":        user.BaseModel.ID,
 	}, nil
+}
+
+func GenerateNationalCode(args ...interface{}) (interface{}, error) {
+	if len(args) < 2 {
+		return nil, errors.New("not enough parameters")
+	}
+	//Example Nomor NIK : 1234567890ABCDEF
+	//12 nomor merupakan kode provinsi
+	//34 nomor merupakan kode kotamadya atau kabupaten kota
+	//56 nomor kode kecamatan
+	//78 nomor tanggal lahir
+	//90 nomor bulan lahir
+	//AB nomor tahun lahir
+	//CDEF nomor registrasi kependudukan
+
+	// Country code 2 characters ID=62
+	// BirthDate 8 characters format yyyyMMDD
+	// Sequence 4 characters
+	date, _ := args[1].(time.Time)
+	var country int
+	if args[0].(string) == "INDONESIA" {
+		country = 62
+	}
+
+	code := fmt.Sprintf("%d%d%02d%02d", country, date.Year(), date.Month(), date.Day())
+
+	var identities []domain.Identity
+	model.GetDatabase().Where("code LIKE ?", code+"%").Find(&identities).Order("code DESC")
+
+	var identity domain.Identity
+	if identities != nil && len(identities) < 0 {
+		identity = identities[0]
+	}
+
+	var seq int
+	if identity.BaseModel.ID != "" {
+		if n, err := strconv.Atoi(identity.Code[10:len(identity.Code)]); err == nil {
+			seq = n + 1
+		} else {
+			return nil, errors.New("is not an integer")
+		}
+	} else {
+		seq = 1
+	}
+
+	return fmt.Sprintf("%s%06d", code, seq), nil
 }
